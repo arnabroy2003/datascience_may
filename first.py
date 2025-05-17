a98090b3401f39f8ee6a910de602fd2d85732c8f
Arnab Roy <arnab.theroy@gmail.com>
2025-05-17 19:57:51 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,42 @@
+# x = 10      #Integer
+# y = 10.5    #Float
+# z = "Name"  #string
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# a = "100.5656"
+
+# print(type(a))
+
+# b = True
+# print(type(b))
+
+# Operations in Python
+
+# 1. Arithmetic Operation
+# x = 10
+# y = 7
+
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x//y)
+# print(x%y)
+# print(x**y)
+
+# 2. Comparison Operators
+x = 10
+y = 7
+z = 7
+
+print(x == y)
+print(y == z)
+
+print(x > y)
+print(x < y)
+
+print(z>=y)
+print(x<=y)
